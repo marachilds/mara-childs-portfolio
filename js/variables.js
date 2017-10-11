0e5ab0cf83911fd2cbdcc3d3bcd4b36975f4fe72
Mara Childs <Mara@Maras-MacBook-Pro-Retina.local>
2017-10-11 17:40:14 +0100

    first commit

---
+++ js/variables.js
@@ -0,0 +1,14 @@
+/* Defining variables to use with pageBuilder.js */
+
+/* Copyright */
+var HTMLfooter = '<p class="copyright">%data%</p>';
+
+/* Navigation Bar */
+var HTMLnavLink = '<li id="navbar"><a href="%data%">';
+var HTMLnavName = '%data%</a></li>';
+
+/* Main Heading */
+var HTMLheading = '<h1>%data%</h1>';
+
+/* Intro */
+var HTMLintro = '<h5>%data%</h5>';
