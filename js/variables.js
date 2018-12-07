ab2c7f6e79d66fb8858a0be8421a0c6b41a0e98a
Mara Childs <marachilds@Mara-Childs-MacBook-Pro.local>
2018-12-07 12:44:51 -0800

    merging 3.1 with 3.0 for version history

--- js/variables.js
+++ js/variables.js
@@ -12,3 +12,18 @@ var HTMLheading = '<h1>%data%</h1>';
 
 /* Intro */
 var HTMLintro = '<h5>%data%</h5>';
+
+/* About Page */
+
+/* Music and Books Section */
+var HTMLitemURL = '<p class="body"><a href="%data%" target="_blank">';
+var HTMLitemTitle = '<em>%data%,</em>';
+var HTMLitemAuthor = '</a> %data%</p>';
+
+/* Movies and TV Section */
+var HTMLmovieURL = '<p class="body"><a href="%data%" target="_blank">';
+var HTMLmovieTitle = '<em>%data%</em></a></p>';
+
+/* Noun Links: People, Studios, Thoughts */
+var HTMLnounURL = '<p class="body"><a href="%data%" target="_blank">';
+var HTMLnounName = '%data%</a></p>'
